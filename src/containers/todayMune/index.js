69af945818124f25ce84a800ec303b6ee4c4ad88
mocaimei <mocaimei@mytophome.com>
2018-03-09 15:44:44 +0800

    引入--数据展示工具--echarts

--- src/containers/todayMune/index.js
+++ src/containers/todayMune/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
 import style from './index.scss'
-import MyLayout from '../../common/layout'
 
+import MyLayout from '../../common/layout'
+import echarts from 'echarts/lib/echarts'
+import 'echarts/lib/chart/bar'
+import 'echarts/lib/component/tooltip'
+import 'echarts/lib/component/title'
 
 class TodayMune extends Component{
     constructor(props){
@@ -10,10 +14,30 @@ class TodayMune extends Component{
         $("body").css("backgroundImage","url('')");
     }
 
+    componentDidMount(){
+        //初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+        //绘制图表
+        myChart.setOption({
+            title: { text: 'ECharts 入门示例'},
+            tooltip: {},
+            xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        });
+    }
+
     render(){
         return(
             <div>
                 <MyLayout>
+                    <div id="main" style={{width:400,height:400}}></div>
                 </MyLayout>
             </div>
         )
